e97644458c29819d43f760c997b2f6d3916e0378
Roshan Raju Daniel <91588500+ROSHAN-RAJU-DANIEL@users.noreply.github.com>
2023-11-01 10:23:12 +0530

    added some fixes to trivia-quiz

--- trivia-quiz/src/components/Questions.js
+++ trivia-quiz/src/components/Questions.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import '../styles/question.css'
 
 const Question = ({ question, onAnswerSubmit, Next, isLastQuestion }) => {
     const [selectedAnswer, setSelectedAnswer] = useState("");
@@ -20,33 +21,31 @@ const Question = ({ question, onAnswerSubmit, Next, isLastQuestion }) => {
     return (
         <div className="question">
             <h2>{question.question}</h2>
-            <ul>
-                {question.options.map((option, index) => (
-                    <li key={index}>
-                        <label>
-                            <input
-                                type="radio"
-                                value={option}
-                                checked={selectedAnswer === option}
-                                onChange={() => handleOptionSelect(option)}
-                                disabled={submitted}
-                            />
-                            <span
-                                style={{
-                                    color:
-                                        submitted && option === question.correctAnswer
-                                            ? "green"
-                                            : submitted && selectedAnswer === option
-                                                ? "red"
-                                                : "inherit",
-                                }}
-                            >
-                                {option}
-                            </span>
-                        </label>
-                    </li>
-                ))}
-            </ul>
+            {question.options.map((option, index) => (
+                <div key={index}>
+                    <label>
+                        <input
+                            type="radio"
+                            value={option}
+                            checked={selectedAnswer === option}
+                            onChange={() => handleOptionSelect(option)}
+                            disabled={submitted}
+                        />
+                        <span
+                            style={{
+                                color:
+                                    submitted && option === question.correctAnswer
+                                        ? "green"
+                                        : submitted && selectedAnswer === option
+                                            ? "red"
+                                            : "inherit",
+                            }}
+                        >
+                            {option}
+                        </span>
+                    </label>
+                </div>
+            ))}
             <button onClick={submitAnswer} disabled={submitted}>
                 Submit
             </button>
